1aff4dbc1306084099e71b41f570cbf00d081ffb
Vibi Sreenivasan <mail.vibi@gmail.com>
2022-10-26 22:53:35 +0200

    use already available feature to set wallpaper from url

--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,12 @@ use process_path::get_executable_path;
 use wallpaper;
 
 fn main() {
+    println!("cache dir {:#?}", dirs::cache_dir());
     const APOD_URL : &str = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
     const URL_KEY: &str = "hdurl";
     const WAIT_TIME_SEC : u64 = 60 * 60 * 5;
-    const IMAGEFILE : &str = "imgfile.jpg";
-    let path : PathBuf; 
-    if let Some(p) = get_executable_path() {
-        path = p;
-    } else {
-        panic!("");
-    };
 
-    let imgfile = path.parent().unwrap().join(IMAGEFILE);
-    println!("{:?}", imgfile);
     loop {
-        let mut oldurl : String;
         let res = reqwest::blocking::get(APOD_URL);
         if let Err(e) = res {
             println!("Received error : {}", e);
@@ -28,19 +19,8 @@ fn main() {
             let b = res.unwrap().text().unwrap();
             println!("Recived Okay data {}", b);
             let v: Value  = serde_json::from_str(b.as_str()).unwrap();
-            if v[URL_KEY] != Value::Null {
-                println!("{:?}", v[URL_KEY]);
-            }
-            let res = reqwest::blocking::get(v[URL_KEY].as_str().unwrap());
-            if let Err(e) = res {
-                println!("Received error {}", e);
-            } else {
-                println!("received file {} - > res {:?}", v[URL_KEY], res);
-                {
-                    let mut ifile = File::create(imgfile.clone()).unwrap();
-                    res.unwrap().copy_to(&mut ifile).unwrap();
-                }
-                wallpaper::set_from_path(imgfile.to_str().unwrap()).unwrap();
+            if let Value::String(url) = v[URL_KEY].clone() {
+                wallpaper::set_from_url(url.as_str());
             }
         }
         thread::sleep(Duration::from_secs(WAIT_TIME_SEC));
